9223297b871c02c8d5f1e2fc336bdb50d43b7b83
chakr0une <soufianbenchakroune7@gmail.com>
2024-06-18 18:04:20 -0700

    fix tasks 6 and 11, 13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const numberArgs = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((x, y) => x - y);
+  console.log(numberArgs[numberArgs.length - 2]);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+for (const lang of langs) {
+  console.log(lang);
 }
